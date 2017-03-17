c998981dfc4893a4083a647c2cb8440aee613423
Tim Gabets <tim@gabets.ru>
2017-03-17 10:48:14 +0300

    ISO8583.Dump() -> isoTools.dump()

--- py8583/isoTools.py
+++ py8583/isoTools.py
@@ -1,12 +1,38 @@
-from ISO8583 import Dump
 from time import gmtime, strftime
+
+def dump(data):
+    """
+    Memdump with no title
+    """
+    i = 1
+
+    if( isinstance(data, bytes) == False ):
+        raise TypeError("Expected bytes for data")
+
+    dump = '\t'
+    
+    for c in data:
+        try: # python 3
+            dump += '{:02x} '.format(c) 
+        except: # python 2.x
+            dump += '{:02x} '.format(ord(c))
+        
+        if(i % 16 == 0):
+            dump += '\n\t'
+        i+=1
+       
+    return dump
+
  
 def get_timestamp(t=None):
     """
+    Get current timestamp
+    TODO: get milliseconds
     """
     return strftime('%H:%M:%S', gmtime())
+
 
 def trace(title, data):
     """
     """
-    print('\n{} {}\n{}'.format(get_timestamp(), title, Dump(data)))
+    print('\n{} {}\n{}'.format(get_timestamp(), title, dump(data)))
