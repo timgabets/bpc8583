14874bd16b993c83829a8b3b59b8860e24f76b83
Tim Gabets <tim@gabets.ru>
2017-04-11 14:06:27 +0300

    manual_purchase transaction

--- bpc8583/transactions.py
+++ bpc8583/transactions.py
@@ -1,8 +1,9 @@
 import random
 
 from ISO8583 import ISO8583
-from isoTools import trace, get_datetime
+from isoTools import trace, get_datetime_with_year, get_datetime
 from py8583spec import IsoSpec, IsoSpec1987BPC
+from datetime import datetime
 
 def echo_test(terminal_id, merchant_id):
     """
@@ -33,7 +34,7 @@ def balance_inquiry(card, terminal_id, merchant_id, currency_code):
     IsoMessage.FieldData(4, 0)
     IsoMessage.FieldData(7, get_datetime())
     IsoMessage.FieldData(11, random.randint(0, 999999))
-    IsoMessage.FieldData(12, 104956)
+    IsoMessage.FieldData(12, get_datetime_with_year())
     IsoMessage.FieldData(13, 1216)
     IsoMessage.FieldData(22, 20)
     IsoMessage.FieldData(24, 100)
@@ -45,3 +46,29 @@ def balance_inquiry(card, terminal_id, merchant_id, currency_code):
 
     IsoMessage.Print()
     return IsoMessage.BuildIso()
+
+
+def manual_purchase(card, terminal_id, merchant_id, currency_code):
+    """
+    Manual Purchase
+    """
+    IsoMessage = ISO8583(IsoSpec=IsoSpec1987BPC())            
+    IsoMessage.MTI("0200")
+        
+    IsoMessage.FieldData(2, card.get_card_number())
+    IsoMessage.FieldData(3, 000000)
+    IsoMessage.FieldData(4, 550)
+    IsoMessage.FieldData(7, get_datetime())
+    IsoMessage.FieldData(11, random.randint(0, 999999))
+    IsoMessage.FieldData(12, get_datetime_with_year())
+    IsoMessage.FieldData(14, card.get_expiry_date)
+    IsoMessage.FieldData(22, 16)
+    IsoMessage.FieldData(24, 100)
+    IsoMessage.FieldData(25, 0)
+    IsoMessage.FieldData(41, terminal_id)
+    IsoMessage.FieldData(42, merchant_id)
+    IsoMessage.FieldData(49, currency_code)
+
+    IsoMessage.Print()
+    return IsoMessage.BuildIso()
+
