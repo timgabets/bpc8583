2a2bb15f2325ccb9b0b5a6acc1184fa60b7bfdf9
Tim Gabets <tim@gabets.ru>
2017-04-13 09:11:37 +0300

    DE 52 length changed from 8 to 16

--- bpc8583/py8583spec.py
+++ bpc8583/py8583spec.py
@@ -383,7 +383,7 @@ ContentTypes['1987'] = {
     49 :  { 'ContentType':'an',    'MaxLen': 3,   'LenType': LT.FIXED },
     50 :  { 'ContentType':'an',    'MaxLen': 3,   'LenType': LT.FIXED },
     51 :  { 'ContentType':'an',    'MaxLen': 3,   'LenType': LT.FIXED },
-    52 :  { 'ContentType':'b',     'MaxLen': 8,   'LenType': LT.FIXED },
+    52 :  { 'ContentType':'b',     'MaxLen': 16,   'LenType': LT.FIXED },
     53 :  { 'ContentType':'n',     'MaxLen': 16,  'LenType': LT.FIXED },
     54 :  { 'ContentType':'an',    'MaxLen': 120, 'LenType': LT.LLLVAR},
     55 :  { 'ContentType':'ans',   'MaxLen': 999, 'LenType': LT.LLLVAR},
