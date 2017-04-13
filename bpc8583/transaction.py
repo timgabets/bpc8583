8131e4095fc9a5fec9f00af4afce6e22943709a7
Tim Gabets <tim@gabets.ru>
2017-04-13 17:31:42 +0300

    transaction PIN and amount setters

--- bpc8583/transaction.py
+++ bpc8583/transaction.py
@@ -6,10 +6,12 @@ from py8583spec import IsoSpec, IsoSpec1987BPC
 from datetime import datetime
 
 class Transaction():
-    def __init__(self, type, card, term, PIN=None, amount=None):
+    def __init__(self, type, card, term):
         """
         """
         self.IsoMessage = ISO8583(IsoSpec=IsoSpec1987BPC())
+        self.card = card
+        self.term = term
 
         if type == 'echo':
             """
@@ -30,10 +32,10 @@ class Transaction():
             self.IsoMessage.FieldData(4, 0)
             self.IsoMessage.FieldData(12, get_datetime_with_year())
             self.IsoMessage.FieldData(13, 1216)
-            self.IsoMessage.FieldData(22, term.get_pos_entry_mode())
+            self.IsoMessage.FieldData(22, self.term.get_pos_entry_mode())
             self.IsoMessage.FieldData(24, 100)
             self.IsoMessage.FieldData(25, 0)
-            self.IsoMessage.FieldData(35, card.get_track2())
+            self.IsoMessage.FieldData(35, self.card.get_track2())
 
         elif type == 'purchase':
             """
@@ -41,14 +43,13 @@ class Transaction():
             self.IsoMessage = ISO8583(IsoSpec=IsoSpec1987BPC())            
             self.IsoMessage.MTI("0200")
         
-            self.IsoMessage.FieldData(2, card.get_card_number())
+            self.IsoMessage.FieldData(2, self.card.get_card_number())
             self.IsoMessage.FieldData(3, 000000)
-            self.IsoMessage.FieldData(4, int(amount))
             self.IsoMessage.FieldData(12, get_datetime_with_year())
-            self.IsoMessage.FieldData(22, term.get_pos_entry_mode())
+            self.IsoMessage.FieldData(22, self.term.get_pos_entry_mode())
             self.IsoMessage.FieldData(24, 100)
             self.IsoMessage.FieldData(25, 0)
-            self.IsoMessage.FieldData(35, card.get_track2())
+            self.IsoMessage.FieldData(35, self.card.get_track2())
 
         else:
             return
@@ -56,17 +57,47 @@ class Transaction():
         # Common message fields:
         self.IsoMessage.FieldData(7, get_datetime())
         self.IsoMessage.FieldData(11, get_stan())
-        self.IsoMessage.FieldData(41, term.get_terminal_id())
-        self.IsoMessage.FieldData(42, term.get_merchant_id())
-        self.IsoMessage.FieldData(49, term.get_currency_code())
-        if PIN:
-            self.IsoMessage.FieldData(52, term.get_encrypted_pin(PIN, card.get_card_number()))
+        self.IsoMessage.FieldData(41, self.term.get_terminal_id())
+        self.IsoMessage.FieldData(42, self.term.get_merchant_id())
+        self.IsoMessage.FieldData(49, self.term.get_currency_code())
 
-        self.IsoMessage.Print()
-        self.data = self.IsoMessage.BuildIso()
+        self.rebuild()
+
 
     def get_data(self):
+        """
+        """
         return struct.pack("!H", len(self.data)) + self.data
 
-    def set_pin(self, PIN):
-        pass
+
+    def rebuild(self):
+        """
+        Rebuild IsoMessage (e.g. after field data change)
+        """
+        self.data = self.IsoMessage.BuildIso()
+
+
+    def trace(self):
+        """
+        """
+        self.IsoMessage.Print()
+
+
+    def set_PIN(self, PIN):
+        """
+        """
+        if PIN:
+            self.IsoMessage.FieldData(52, self.term.get_encrypted_pin(PIN, self.card.get_card_number()))
+            self.rebuild()
+
+
+    def set_amount(self, amount):
+        """
+        Set transaction amount
+        """
+        if amount:
+            self.IsoMessage.FieldData(4, int(amount))
+            self.rebuild()
+
+
+        
