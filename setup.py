ee691033fd45b86a2650513956babd44dfb6ad5e
Tim Gabets <tim@gabets.ru>
2017-05-04 18:20:08 +0300

    Updated RespCodeDescriptions

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='bpc8583',
-      version='1.20',
+      version='1.21',
       
       description='BPC\'s flavour of ISO8583',
       long_description=open('README.md').read(),
