d82522c2334da9489d33732fd6d6be8a57a2946e
Tim Gabets <tim@gabets.ru>
2017-05-29 12:05:41 +0300

    v1.38 isoClient and isoHost moved to examples/

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='bpc8583',
-      version='1.37',
+      version='1.38',
       
-      description='BPC\'s flavour of ISO8583 library and toolset',
+      description='ISO8583 library and tools (BPC\'s flavour)',
       long_description=open('README').read(),
       
       classifiers=[
