97f2dc563a223c82c0aa84beebe0871bd4577db6
Tim Gabets <tim@gabets.ru>
2017-06-07 12:32:29 +0300

    v1.48 get_random_amount removed, using direct call to random.randint()

--- bpc8583/tools.py
+++ bpc8583/tools.py
@@ -58,15 +58,6 @@ def get_random_hex(length):
     return hexify(random.randint(pow(2, length*2), pow(2, length*4)))[0:length]
 
 
-def get_random_amount(min=None, max=None, step=None):
-    _min = min if min else 0
-    _max = max if max else 1000000
-    if step:
-        return round(random.randint(_min, _max) / step) * step
-    else:
-        return random.randint(_min, _max)
-
-
 def get_response(_code):
     """
     Return xx1x response for xx0x codes (e.g. 0810 for 0800)
--- examples/isoClient.py
+++ examples/isoClient.py
@@ -4,6 +4,7 @@ import sys
 import os
 import getopt
 import time
+import random
 import xml.etree.ElementTree as ET
 from collections import OrderedDict
 
@@ -12,7 +13,7 @@ from bpc8583.spec import IsoSpec, IsoSpec1987BPC
 from bpc8583.terminal import Terminal
 from bpc8583.card import Card
 from bpc8583.transaction import Transaction
-from bpc8583.tools import trace_passed, trace_failed, get_random_amount
+from bpc8583.tools import trace_passed, trace_failed
 
 
 class isoClient:
@@ -186,23 +187,20 @@ def parse_transaction_item(trxn, term, cards):
         for attrib in trxn:
             if attrib.tag.lower() == 'amount':
                 if attrib.text == 'random':
-                    min = None
                     try:
-                        int(attrib.attrib['min'])
+                        min = int(attrib.attrib['min'])
                     except KeyError:
-                        pass
-                    max = None
+                        min = 0
                     try:
                         max = int(attrib.attrib['max'])
                     except KeyError:
-                        pass
-                    step = None
+                        max = 100
                     try:
-                        step = int(attrib.attrib['step'])
+                        exponent = int(attrib.attrib['exponent'])
                     except KeyError:
-                        pass
+                        exponent = 1
 
-                    random_amount = get_random_amount(min, max, step)
+                    random_amount = random.randint(min, max) * exponent
                     t.set_amount(random_amount)
                 else:
                     t.set_amount(attrib.text)
