24eab0aade35aa99ea70aefc58e5be1b1f85de6d
Tim Gabets <tim@gabets.ru>
2017-08-22 12:39:28 +0300

    Version fix

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='bpc8583',
-      version='1.52',
+      version='1.54',
       
       description='ISO8583 library and tools (BPC\'s flavour)',
       long_description=open('README.rst').read(),
